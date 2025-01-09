6dbef5567775ba5bff87f2954fd4e2b20ec95fd5
SAMADHAN DNYANESHWAR GHORPADE <136071800+SamadhanGh@users.noreply.github.com>
2025-01-09 23:25:59 +0530

    Update AasFun_argument.java

--- Array_c/AasFun_argument.java
+++ Array_c/AasFun_argument.java
@@ -3,16 +3,12 @@
 package Array_c;
 
 public class AasFun_argument {
-
     public static void update(int marks[]){
         for(int i=0;i<marks.length;i++){
 
             marks[i] = marks[i] + 1;
 
-
         }
-
-
 
     }
     public static void main(String[] args) {
